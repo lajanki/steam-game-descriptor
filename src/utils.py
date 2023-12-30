fcd52eaefad44d05f6ac98bcdf5c3a31ba689360
Lauri Ajanki <lauri.ajanki@gmail.com>
2023-12-30 20:21:29 +0200

    add logging

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
+import logging
 import os
 import os.path
-import json
 
 from google.cloud import storage
 
@@ -26,13 +26,18 @@ def download_from_gcs(bucket, path):
 
 def download_descriptions_as_text():
 	"""Download all descriptions currently in temp bucket."""
-	blobs = gcs_client.list_blobs(TEMP_BUCKET, prefix="steam_game_descriptor/descriptions")
+	prefix = "steam_game_descriptor/descriptions"
+	blobs = gcs_client.list_blobs(TEMP_BUCKET, prefix=prefix)
 	results = []
 
+	count = 0
 	for blob in blobs:
+		count += 1
 		data_string = blob.download_as_bytes().decode("utf8")
 		results.append(data_string)
 	
+	logging.info("Loaded %d descriptions from gs://%s/%s", count, TEMP_BUCKET, prefix)
+
 	return " ".join(results)
 
 def get_text_file(filename):
