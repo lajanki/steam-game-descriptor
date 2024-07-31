b037c80e9f46ca25988e19d888dc4ea79173da1d
Lauri Ajanki <lauri.ajanki@gmail.com>
2024-07-31 20:33:39 +0300

    test: add test for requirements parsing

--- test/test_parser.py
+++ test/test_parser.py
@@ -6,6 +6,7 @@ with patch("google.cloud.storage.Client"):
 
 def test_description_parsing_on_html_tags():
     """Are html tags remoevd and whitespace added between where a newline would be originalyl rendered?"""
+    ## Simple example with headers and emphasis tags
     html = '<h2 class="bb_tag"><strong>INTRODUCTION</strong></h2><strong>Fight against all odds and repel an entire army from the great city of Itenbaal! \
     </strong><br><br>Without warning, an experimental battalion composed entirely of humadroids went haywire during a sandstorm and turned on their \
     very own place of creation.'
@@ -17,7 +18,7 @@ def test_description_parsing_on_html_tags():
     # Compare word list to avoid whitespace differences
     assert parser._html_string_to_text(html).split() == expected.split()
 
-    # More tag filtering with an img src reference
+    ## More tag filtering with an img src reference
     html = '</strong><br><br>Newgame+ is more than just a higher difficulty, more vicous enemies and boss encounters awaits you ahead! \
     <br><br><img src="https://cdn.akamai.steamstatic.com/steam/apps/359970/extras/Encounter.png?t=1608617814" />'
     
@@ -49,3 +50,100 @@ def test_description_parsing_on_regular_string():
     very own place of creation.'
 
     assert parser._html_string_to_text(text).split() == text.split()
+
+def test_requirements_parsing():
+    """Test system requirement extraction on html strings."""
+    ## pc_requirements only; minimum and recommended
+    data = {
+        "pc_requirements": {
+            "minimum":"""
+            <strong>Minimum:</strong><br>
+            <ul class="bb_ul">
+                <li><strong>OS *:</strong> Windows 7/8.1/10 (64-bit versions)<br></li>
+                <li><strong>Processor:</strong> Intel Core i5-2400/AMD FX-8320 or better<br></li>
+                <li><strong>Memory:</strong> 8 GB RAM<br></li>
+                <li><strong>Graphics:</strong> NVIDIA GTX 670 2GB/AMD Radeon HD 7870 2GB or better<br></li>
+                <li><strong>Storage:</strong> 55 GB available space<br></li>
+                <li><strong>Additional Notes:</strong> Requires Steam activation and broadband internet connection for Multiplayer and SnapMap</li>
+            </ul>""",
+            "recommended":"""
+                <strong>Recommended:</strong><br>
+                <ul class="bb_ul">
+                    <li><strong>OS *:</strong> Windows 7/8.1/10 (64-bit versions)<br></li>
+                    <li><strong>Processor:</strong> Intel Core i7-3770/AMD FX-8350 or better<br></li>
+                    <li><strong>Memory:</strong> 8 GB RAM<br></li><li><strong>Graphics:</strong> NVIDIA GTX 970 4GB/AMD Radeon R9 290 4GB or better<br></li>
+                    <li><strong>Storage:</strong> 55 GB available space<br></li>
+                    <li><strong>Additional Notes:</strong> Requires Steam activation and broadband internet connection for Multiplayer and SnapMap</li>
+                </ul>""",
+        },
+        "mac_requirements": [],
+        "linux_requirements": [],
+    }
+
+    expected = {
+        "OS": {"Windows 7/8.1/10 (64-bit versions)"},
+        "Processor": {
+            "Intel Core i5-2400/AMD FX-8320 or better",
+            "Intel Core i7-3770/AMD FX-8350 or better",
+        },
+        "Memory": {"8 GB RAM"},
+        "Graphics": {
+            "NVIDIA GTX 670 2GB/AMD Radeon HD 7870 2GB or better",
+            "NVIDIA GTX 970 4GB/AMD Radeon R9 290 4GB or better",
+        },
+        "Storage": {"55 GB available space"},
+        "Additional Notes": {
+            "Requires Steam activation and broadband internet connection for Multiplayer and SnapMap"
+        },
+    }
+
+    ## More complex example; minimum requirements for multiple OS
+    data = {
+        "pc_requirements": {
+            "minimum": """
+                <strong>Minimum:</strong><br>
+                <ul class="bb_ul">
+                    <li><strong>OS *:</strong> Windows 7/8 64 bit or higher<br></li>
+                    <li><strong>Processor:</strong> Intel Core2 Duo or better<br></li>
+                    <li><strong>Memory:</strong> 4 GB RAM<br></li><li><strong>Graphics:</strong> DirectX 10.1 compatible 512 MB<br></li>
+                    <li><strong>DirectX:</strong> Version 11<br></li><li><strong>Storage:</strong> 700 MB available space<br></li>
+                    <li><strong>Sound Card:</strong> DirectX 9.0c compatible<br></li>
+                    <li><strong>Additional Notes:</strong> Mouse recommended. Passing can be difficult with the touch pad on some laptops.</li>
+                </ul>"""
+        },
+        "mac_requirements": {
+            "minimum": """
+                <strong>Minimum:</strong><br>
+                <ul class="bb_ul">
+                    <li><strong>OS:</strong> Mac OS X 10.6<br></li>
+                    <li><strong>Processor:</strong> Quad core Intel or AMD processor, 2.5 GHz +<br></li>
+                    <li><strong>Memory:</strong> 4 GB RAM</li>
+                </ul>"""
+        },
+        "linux_requirements": {
+            "minimum": """
+                <strong>Minimum:</strong><br>
+                <ul class="bb_ul">
+                    <li><strong>Processor:</strong> Quad core Intel or AMD processor, 2.5 GHz +<br></li>
+                    <li><strong>Memory:</strong> 4 GB RAM</li>
+                </ul>"""
+        },
+    }
+
+    expected = {
+        "OS": {"Mac OS X 10.6", "Windows 7/8 64 bit or higher"},
+        "Processor": {
+            "Intel Core2 Duo or better",
+            "Quad core Intel or AMD processor, 2.5 GHz +",
+        },
+        "Memory": {"4 GB RAM"},
+        "Graphics": {"DirectX 10.1 compatible 512 MB"},
+        "DirectX": {"Version 11"},
+        "Storage": {"700 MB available space"},
+        "Sound Card": {"DirectX 9.0c compatible"},
+        "Additional Notes": {
+            "Mouse recommended. Passing can be difficult with the touch pad on some laptops."
+        },
+    }
+
+    assert parser.extract_requirements(data) == expected
