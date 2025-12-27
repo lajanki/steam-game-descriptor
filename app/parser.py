31032255f699c31a2d19822fa7b943d92b699356
Lauri Ajanki <lauri.ajanki@gmail.com>
2025-12-27 12:35:26 +0200

    feat: replace true app list with random id sample

--- app/parser.py
+++ app/parser.py
@@ -41,8 +41,8 @@ def upload_description_batch(batch_size=200):
 	Args:
 		batch_size (int): sample size of descriptions to parse.
 	"""
-	app_id_list = _get_app_id_list()
-	sample = random.sample(app_id_list, batch_size)
+	app_id_batch = get_app_id_batch(batch_size)
+
 	TEMP_BUCKET_PREFIX = os.environ["TEMP_BUCKET_PREFIX"]
 
 	logger.info("Parsing %s descriptions", batch_size)
@@ -50,7 +50,7 @@ def upload_description_batch(batch_size=200):
 		s.params = {"cc": "us", "l": "english"}
 
 		success = 0
-		for app_id in sample:
+		for app_id in app_id_batch:
 			logger.debug("Querying %s?appids=%s", URL, app_id)
 			r = s.get(URL, params={"appids": app_id})
 			r.raise_for_status()
@@ -94,10 +94,38 @@ def upload_description_batch(batch_size=200):
 		TEMP_BUCKET_PREFIX,
 	)
 
-def _get_app_id_list():
-	"""Fetch a list of games on the Steam store.
+
+def get_app_id_batch(batch_size):
+	"""Get a batch of pseudo Steam app ids.
+	
+	While the official Steamworks API provides an endpoint for available apps,
+	we only need a small sample of app ids for training purposes.
+	https://partner.steamgames.com/doc/webapi/IStoreService#GetAppList
+
+	The app ids start from 10 and (at the time of writing) reach to over 4 000 000.
+	Base game ids appear to end with 0.
+
+	The ids returned are not guaranteed to be valid Steam apps.
+
+	Args:
+		batch_size (int): number of items to return
 	Return:
-		list of app ids"""
+		a list of random numeric ids
+	"""
+	return random.sample(range(10, 4_000_000, 10), batch_size)
+
+def _get_app_id_list():
+	"""DEPRECATED
+	The referenced API endpoint appears to be deprecated
+	https://partner.steamgames.com/doc/webapi/isteamapps#GetAppList
+
+	See get_app_id_batch instead.
+
+	Fetch a list of games on the Steam store.
+	Return:
+		list of app ids
+	"""
+	logger.info("Fetching app list from Steam API...")
 	r = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2")
 	app_list = r.json()["applist"]["apps"]
 	# excude trailers, soundtracks and demos
