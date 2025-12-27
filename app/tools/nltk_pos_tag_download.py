6bbc0217b028bf1421b1e872654ae3287a366bc8
Lauri Ajanki <lauri.ajanki@gmail.com>
2025-12-27 13:47:19 +0200

    feat: simplify nltk POS map creation

--- app/tools/nltk_pos_tag_download.py
+++ app/tools/nltk_pos_tag_download.py
@@ -4,24 +4,40 @@ import os.path
 
 import nltk
 
+from app.utils.json_set_encoder import SetEncoder
+
 
 def create_pos_tag_mapping():
-	"""Creating a json mapping of POS tag -> word from the nltk Brown Corpus.
+	"""Create a json mapping of POS tag -> word from the nltk Brown Corpus.
 	https://www.nltk.org/book/ch02.html
-	Note: this required setting up nltk module with the Brown Corpus data, see
-	https://www.nltk.org/install.html
 	
-	This function is intended to be run manually to re-create the mapping data if needed.
-	""" 
+	Requires setting up nltk module with the Brown Corpus data,
+	>>> import nltk
+	>>> nltk.download('brown')
+	>>> nltk.download('universal_tagset')
+	
+	see
+	https://www.nltk.org/install.html
+	"""
+	TAGS_TO_KEEP = [
+		"NOUN",
+		"ADJ",
+		"VERB",
+		"ADV",
+		"NUM",
+		"X"
+	]
+	OUTPUT_FILE = os.path.join("app", "data", "pos_tags.json")
+
 	d = defaultdict(set)
 	tags = nltk.corpus.brown.tagged_words(tagset="universal") 
 	for token in tags:
-		if not token[0].startswith("'"):
-			d[token[1]].add(token[0])
+		if token[1] not in TAGS_TO_KEEP:
+			continue
 
-	# Transform values back to list for json compatibility 
-	for tag in d:
-		d[tag] = list(d[tag])
+		d[token[1]].add(token[0].strip("'"))
 		
-	with open(os.path.join("app", "data", "pos_tags.json"), "w") as f:
-		json.dump(d, f, separators=(",", ":"))
+	with open(OUTPUT_FILE, "w") as f:
+		json.dump(d, f, separators=(",", ":"), cls=SetEncoder)
+
+	print(f"POS map saved as {OUTPUT_FILE}")
